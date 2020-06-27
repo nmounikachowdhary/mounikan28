f1f5ef63ad97abce5b85278bc12341d31f9921c8
nmounikachowdhary <67091421+nmounikachowdhary@users.noreply.github.com>
2020-06-27 15:29:27 +0530

    Add files via upload

---
+++ assignment(27-6-2020)_321910301006_Mounika.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+for fizzbuzz in range(100):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print("fizz")
+        continue
+    elif fizzbuzz % 5 == 0:
+        print("buzz")
+        continue
+    print(fizzbuzz)
+
+
+# In[4]:
+
+
+from itertools import groupby
+def compress(l_nums):
+    return [key for key, group in groupby(l_nums)] 
+n_list = [0, 0, 1, 2, 3, 4, 4, 5, 6, 6, 6, 7, 8, 9, 4, 4 ]
+print("\nAfter removing consecutive duplicates:")
+print(compress(n_list)) 
+
+
+# In[6]:
+
+
+def unique_list(l):
+  x = []
+  for a in l:
+    if a not in x:
+      x.append(a)
+  return x
+
+print(unique_list([1,2,3,3,3,3,4,5,6,7,10])) 
+
+
+# In[9]:
+
+
+def string_test(s):
+    d={"UPPER_CASE":0, "LOWER_CASE":0}
+    for c in s:
+        if c.isupper():
+           d["UPPER_CASE"]+=1
+        elif c.islower():
+           d["LOWER_CASE"]+=1
+        else:
+           pass
+    print ("Original String : ", s)
+    print ("No. of Upper case characters : ", d["UPPER_CASE"])
+    print ("No. of Lower case Characters : ", d["LOWER_CASE"])
+
+string_test('HAI python have a goodday')
+
+
+# In[10]:
+
+
+def test_range(n):
+    if n in range(3,9):
+        print( " %s is in the range"%str(n))
+    else :
+        print("The number is outside the given range.")
+test_range(5)
+
+
+# In[ ]:
+
+
+
+
